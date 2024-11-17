c3c511c79d18b98ad063dca96e0bc8e15c0a5b79
Luis E. Palomares <lepalomares@me.com>
2024-11-16 20:31:52 -0600

    frontend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const pool = require('./db');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 app.get('/nodes', async (req, res) => {
     const result = await pool.query('SELECT * FROM nodes');
